ff5b154331fffdd70eafafb9e9a2f3cba30fe597
play.js <git@gituser.com>
2021-04-29 21:42:25 +0100

    Squirrel  ipad test

--- components/sliders.vue
+++ components/sliders.vue
@@ -1,24 +1,22 @@
 <template>
   <v-carousel v-model="model">
+
     <v-carousel-item
-      v-for="(color, i) in colors"
-      :key="color"
-    >
-      <v-sheet
-        :color="color"
-        height="100%"
-        tile
-      >
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition">
+
         <v-row
           class="fill-height"
           align="center"
-          justify="center"
-        >
+          justify="center">
           <div class="display-3">
             Slide {{ i + 1 }}
           </div>
         </v-row>
-      </v-sheet>
+
     </v-carousel-item>
   </v-carousel>
 </template>
@@ -35,6 +33,20 @@ export default {
       'red',
       'orange',
     ],
+    items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+      ]
   }),
 }
 </script>
