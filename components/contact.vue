d45549e4a445abd529b80b47e75a6575fb6f5c37
jenni <jennruth96@gmail.com>
2022-05-23 11:36:16 +0100

    Contact form

--- components/contact.vue
+++ components/contact.vue
@@ -5,7 +5,19 @@
     </v-card-title>
     <v-card-text>
       <p> Email me on: <a href="mailto:jen@jenergie.co.uk">jen@jenergie.co.uk</a></p>
-      Message me on:
+      <v-card light>
+
+
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSf-l3Yt44RVm_iGg3SC8eugXI8mKL2b0-xT1GpNxkOpF3hciQ/viewform?embedded=true"
+          width="100%" height="500"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </v-card>
     </v-card-text>
   </v-card>
 </template>
